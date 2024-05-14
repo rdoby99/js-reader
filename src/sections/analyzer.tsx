a2b3435b804852b0f582079c760377ee0d5ba3e1
Ramona Doby <74220362+rdoby99@users.noreply.github.com>
2024-05-14 17:08:32 -0400

    ve.js

--- src/sections/analyzer.tsx
+++ src/sections/analyzer.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 
 export default function Analyzer() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState([]);
+
+  const analyzeText = () => {
+    // const veInstance = new Ve("ichi1");
+    // const results = veInstance.analyze(input);
+    // setOutput(results);
+
+    new Ve("ichi1").words(input, function (words) {
+      for (i in words) {
+        var word = words[i];
+        console.log(word.lemma + "/" + word.part_of_speech);
+      }
+    });
+  };
+
   return (
     <section className="grid grid-cols-2 gap-16 items-center w-full py-16 px-16 min-h-[70vh]">
       <div className="text-center col-span-1 flex flex-col gap-4">
@@ -13,8 +29,17 @@ export default function Analyzer() {
         </p>
       </div>
       <div className="col-span-1 flex flex-col gap-4">
-        <Textarea placeholder="Input Japanese text here. ここに日本語のテキストを入力してください." />
-        <Button>Analyze</Button>
+        <Textarea
+          placeholder="Input Japanese text here. ここに日本語のテキストを入力してください..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button onClick={analyzeText}>Analyze</Button>
+        <ul>
+          {output.map((item, index) => (
+            <li key={index}>{item.surface}</li>
+          ))}
+        </ul>
       </div>
     </section>
   );
