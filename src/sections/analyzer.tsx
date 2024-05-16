d8fac1669694bac78119d26e1205642e1e5ab3e6
Ramona Doby <74220362+rdoby99@users.noreply.github.com>
2024-05-16 17:48:24 -0400

    Python server

--- src/sections/analyzer.tsx
+++ src/sections/analyzer.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 
 export default function Analyzer() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState([]);
+
+  const analyzeText = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/words?text=${encodeURIComponent(input)}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setOutput(data.words);
+    } catch (error) {
+      console.error("An error occurred:", error);
+      // You can also set the error message in your component's state to display it in your UI
+      // setError(error.message);
+    }
+  };
+
   return (
     <section className="grid grid-cols-2 gap-16 items-center w-full py-16 px-16 min-h-[70vh]">
       <div className="text-center col-span-1 flex flex-col gap-4">
@@ -13,8 +41,17 @@ export default function Analyzer() {
         </p>
       </div>
       <div className="col-span-1 flex flex-col gap-4">
-        <Textarea placeholder="Input Japanese text here. ここに日本語のテキストを入力してください." />
-        <Button>Analyze</Button>
+        <Textarea
+          placeholder="Input Japanese text here. ここに日本語のテキストを入力してください..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button onClick={analyzeText}>Analyze</Button>
+        <ul>
+          {output.map((item, index) => (
+            <li key={index}>{item.surface}</li>
+          ))}
+        </ul>
       </div>
     </section>
   );
